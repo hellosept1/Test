8b799d10fd2c689a4543f3c89179e9e560cfc5c1
hellosept1 <hellosept1@gmail.com>
2025-08-17 13:01:00 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
-    <footer style={{ padding: '2rem', textAlign: 'center', background: '#222', color: '#fff' }}>
+    <footer style={{ padding: '2rem', textAlign: 'center', background: '#111', color: '#fff' }}>
       <p>&copy; 2025 Whoso Live. All rights reserved.</p>
+      <p style={{ fontSize: '0.8rem' }}>Chess-inspired landing experience</p>
     </footer>
   )
 }
