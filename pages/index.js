c26410d7a680001cfa788855b1ce1af24f987c01
hellosept1 <hellosept1@gmail.com>
2025-08-17 12:47:16 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Features from '../components/Features'
+import About from '../components/About'
+import CTA from '../components/CTA'
+import Footer from '../components/Footer'
+
 export default function Home() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Whyso.live Landing Page Test</h1>
-      <p>If you see this, deployment works!</p>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <Features />
+      <About />
+      <CTA />
+      <Footer />
+    </>
   )
 }
