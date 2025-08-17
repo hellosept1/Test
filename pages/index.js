ae8c9a6fde7780c98805044fdb12ebd4f8294668
hellosept1 <hellosept1@gmail.com>
2025-08-17 12:29:40 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Whyso.live Landing Page Test</h1>
+      <p>If you see this, deployment works!</p>
+    </div>
+  )
+}
